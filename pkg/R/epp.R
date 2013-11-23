dd918cb8143da664222adb883e3ad15eefa04125
valcu (remove_account remove_account) <valcu@orn.mpg.de>
2013-11-23 16:23:22 +0000

    updated epp.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mvalcu@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/epp.R
+++ pkg/R/epp.R
@@ -2,7 +2,7 @@
 setClass("epp", representation(
 	breedingDat     = "SpatialPointsBreeding", 
 	polygonsDat    	= "SpatialPolygonsDataFrame", 
-	eppPairs      	= "data.frame", # TODO should extend data.frame
+	eppPairs      	= "data.frame", 
 	rank 			= "numeric", 
 	EPP				= "data.frame"
 	),
@@ -10,6 +10,9 @@ setClass("epp", representation(
 	validity = function(object)	{
 		if (object@rank < 1)
 			stop("rank must be greater than 0.")	
+	if(! identical(polygonsDat@data[, 1], breedingDat@id))
+    stop( dQuote("polygonsDat@data[, 1]"), " does not match ",  dQuote("breedingDat@id") )
+      
 		return(TRUE)
 		}
  )
@@ -17,42 +20,41 @@ setClass("epp", representation(
 
 epp <- function(breedingDat, polygonsDat = DirichletPolygons(breedingDat), eppPairs, rank = 3) { 
 
-  # example
-    set.seed(1310)
-	b = data.frame(id = 1:5, x = rnorm(5), y = rnorm(5), male = paste0("m",1:5), female =  paste0("f",1:5), xx = rnorm(5), stringsAsFactors=FALSE  )  
-    breedingDat = SpatialPointsBreeding(b )
-    polygonsDat = DirichletPolygons(breedingDat)
-    eppPairs = data.frame(male = c("m1", "m1", "m3", "m5"), female=c("f2", "f3", "f5", "f3") )
     
-    plot(polygonsDat)
-    plot(breedingDat, add = T)
-    
-	#bricks
+	  #bricks
 		nb  = poly2nb(polygonsDat)
 		hnb = higherNeighborsDataFrame(nb, maxlag = rank)
 		b   = data.frame(breedingDat@data, id = breedingDat@id, male = breedingDat@male, female = breedingDat@female)
     
-  # build up epp set
-    d = merge(hnb, b, by = "id")
-    d = merge(d, b, by.x = 'id_neigh', by.y = 'id',  all.x = TRUE, sort = FALSE, suffixes= c("","_neigh") )
-	
-    e = eppPairs
-    e$z = paste(e$male, e$female)
-    e$epp = 1; e$male = NULL; e$female = NULL
-    
-	d$z = paste(d$male, d$female_neigh)
+    # build up epp set
+    d = merge(hnb, b, by = "id") 
+    d = merge(d, b, by.x = 'id_neigh', by.y = 'id',  all.x = TRUE, suffixes= c("_S","_N") )
+	  d$k_S = NULL; d$k_N = NULL
+    d$z = paste(d$male_S, d$female_N)    
+
+    e = data.frame(z = paste(eppPairs$male, eppPairs$female), epp = 1)
+
+
     d = merge(d, e, by = "z", all.x = TRUE)
     d$z = NULL
-	d$epp = as.numeric(!is.na(d$epp))
-	d$k = NULL
-	d$k_neigh = NULL
-	
-	d[d$epp == 1, ]
+    d[is.na(d$epp), "epp"] = 0
+    
+    # fix names
+    names(d) [which(names(d) == "male_S")] = "male"
+    names(d) [which(names(d) == "female_N")] = "female"
+    d$male_N = NULL; d$female_S = NULL    
+    d = d[, union(c("id_neigh", "id", "rank", "male", "female", "epp"), names(d)) ]
+    
+
+    # new
+		new("epp", breedingDat = breedingDat, polygonsDat = polygonsDat, eppPairs = eppPairs, rank = rank, EPP = d)
     
     
     
     
 	}
+
+
 	
 	
 
